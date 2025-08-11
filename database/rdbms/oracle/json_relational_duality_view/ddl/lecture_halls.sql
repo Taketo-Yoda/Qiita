b483f740a600d21da1f128640ae72791352187e1
Taketo-Yoda <exhaust7@hotmail.com>
2025-08-11 15:19:04 +0900

    add relations entity

---
+++ database/rdbms/oracle/json_relational_duality_view/ddl/lecture_halls.sql
@@ -0,0 +1,18 @@
+CREATE TABLE lecture_halls (
+    id VARCHAR2(32) DEFAULT SYS_GUID() PRIMARY KEY,
+    name VARCHAR2(50) NOT NULL,
+    capacity NUMBER(4) NOT NULL
+);
+CREATE TABLE courses (
+    id VARCHAR2(32) DEFAULT SYS_GUID() PRIMARY KEY,
+    title VARCHAR2(50) NOT NULL,
+    lecture_hall_id VARCHAR2(32) NOT NULL,
+    FOREIGN KEY (lecture_hall_id) REFERENCES lecture_halls(id)
+);
+CREATE TABLE enrollments (
+    student_id VARCHAR2(32) NOT NULL,
+    course_id VARCHAR2(32) NOT NULL,
+    PRIMARY KEY (student_id, course_id),
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (course_id) REFERENCES courses(id)
+);
