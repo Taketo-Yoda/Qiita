c384530dd197a5ecaaa9002ebb367bcf944755b0
Taketo-Yoda <exhaust7@hotmail.com>
2025-08-11 14:05:18 +0900

    add atandard select dml and multiple insert

---
+++ database/rdbms/oracle/json_relational_duality_view/dml/students_insert_json_multiple.sql
@@ -0,0 +1,16 @@
+INSERT INTO dv_students VALUES (
+    '[
+        {
+            "first_name": "Emily",
+            "last_name": "Harris",
+            "major": "Biology",
+            "gpa": 4.1
+        },
+        {
+            "first_name": "Frank",
+            "last_name": "Johnson",
+            "major": "Chemistry",
+            "gpa": 3.9
+        }
+    ]'
+);
---
+++ database/rdbms/oracle/json_relational_duality_view/dml/students_select_all.sql
@@ -0,0 +1,11 @@
+SELECT
+    s.id,
+    s.first_name,
+    s.last_name,
+    s.major,
+    s.gpa
+FROM
+    students s
+ORDER BY
+    s.first_name
+;
